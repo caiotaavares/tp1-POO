deb5cd012da7f4b6ee5d6b122b96dfd567a04131
joão <joaopss2001@gmail.com>
2022-11-27 17:47:21 -0300

    interface

---
+++ src/main/java/Controller/CadastroClienteController.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controller;
+
+import view.CadastroCliente;
+/**
+ *
+ * @author João Pedro
+ */
+public class CadastroClienteController {
+    
+     public CadastroClienteController(CadastroCliente view){
+     
+     }
+}
---
+++ src/main/java/Controller/MenuPrincipalController.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controller;
+
+import view.MenuPrincipal;
+/**
+ *
+ * @author João Pedro
+ */
+public class MenuPrincipalController {
+    
+    public MenuPrincipalController(MenuPrincipal view){
+    
+    }
+    
+}
