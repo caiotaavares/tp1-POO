d158a350ae7ccbccb61badd8dbc7c83f34ccd4ae
joão <joaopss2001@gmail.com>
2022-11-28 07:23:32 -0300

    inserindo nova interface

---
+++ src/main/java/Controller/CadastroImovelController.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controller;
+
+import view.CadastroImovel;
+
+/**
+ *
+ * @author João Pedro
+ */
+public class CadastroImovelController {
+
+    public CadastroImovelController(CadastroImovel view) {
+    
+    }
+    
+}
---
+++ src/main/java/Controller/InformacoesImoveisController.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controller;
+
+import view.InformacoesImoveis;
+
+/**
+ *
+ * @author João Pedro
+ */
+public class InformacoesImoveisController {
+
+    public InformacoesImoveisController(InformacoesImoveis view) {
+    }
+    
+}
