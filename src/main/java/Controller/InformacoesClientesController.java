a098329d058385635c33c1bc6581e1823a5d6541
joão <joaopss2001@gmail.com>
2022-11-28 07:29:56 -0300

    arquivos corretos

---
+++ src/main/java/Controller/InformacoesClientesController.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controller;
+
+import view.InformacoesClientes;
+
+/**
+ *
+ * @author João Pedro
+ */
+public class InformacoesClientesController {
+
+    public InformacoesClientesController(InformacoesClientes view) {
+    }
+    
+}
